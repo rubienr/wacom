adbfd0d4a1111ed00d4294c8eb3d2adf13d040e8
Raoul Rubien <rubienr@sbox.tugraz.at>
2022-11-13 19:32:54 +0100

    refined cli arguments and help text

--- src/xbindkeys_utils.py
+++ src/xbindkeys_utils.py
@@ -1,40 +1,31 @@
 import subprocess
 import tempfile
-from enum import Enum
-
-
-class XBindKeysRunMode(Enum):
-    START_FOREGROUND = "fg"
-    START_BACKGROUND = "bg"
-    KILL = "ki"
-    RELOAD = "re"
 
 
 def _run_subprocess(args: str, **kwargs) -> subprocess.CompletedProcess:
     return subprocess.run(args, shell=True, text=True, **kwargs)
 
 
-def run_xbindkeys(config: str, run_mode: str):
-    print(f"invoke xbindkeys (mode={run_mode})")
-    if run_mode == XBindKeysRunMode.START_BACKGROUND.value:
-        xbindkeys_cfg = tempfile.NamedTemporaryFile(mode='w+b', prefix="xbindkeys-cfg-for-xsetwacom-", suffix=".tmp", delete=False)
+def xbindkeys_start_foreground(config: str) -> None:
+    with tempfile.NamedTemporaryFile(mode='w+b', prefix="xbindkeys-cfg-for-xsetwacom-", suffix=".tmp", delete=True) as xbindkeys_cfg:
         xbindkeys_cfg.write(config.encode())
         xbindkeys_cfg.flush()
         print(f"temporary xbindkeys config {xbindkeys_cfg.name}")
-        _run_subprocess(f"xbindkeys --file {xbindkeys_cfg.name}", check=True)
-        # TODO: tmp config file is never removed
-    elif run_mode == XBindKeysRunMode.START_FOREGROUND.value:
-        with tempfile.NamedTemporaryFile(mode='w+b', prefix="xbindkeys-cfg-for-xsetwacom-", suffix=".tmp", delete=True) as xbindkeys_cfg:
-            xbindkeys_cfg.write(config.encode())
-            xbindkeys_cfg.flush()
-            print(f"temporary xbindkeys config {xbindkeys_cfg.name}")
-            _run_subprocess(f"xbindkeys --file {xbindkeys_cfg.name} --verbose --nodaemon")
-    elif run_mode == XBindKeysRunMode.KILL.value:
-        _run_subprocess("killall xbindkeys")
-    elif run_mode == XBindKeysRunMode.RELOAD.value:
-        _run_subprocess("killall -HUP xbindkeys")
-    else:
-        print(f"unknown run_mode={run_mode}")
-        return False
+        _run_subprocess(f"xbindkeys --file {xbindkeys_cfg.name} --verbose --nodaemon")
 
-    return True
+
+def xbindkeys_start_background(config: str) -> None:
+    xbindkeys_cfg = tempfile.NamedTemporaryFile(mode='w+b', prefix="xbindkeys-cfg-for-xsetwacom-", suffix=".tmp", delete=False)
+    xbindkeys_cfg.write(config.encode())
+    xbindkeys_cfg.flush()
+    print(f"temporary xbindkeys config {xbindkeys_cfg.name}")
+    _run_subprocess(f"xbindkeys --file {xbindkeys_cfg.name}", check=True)
+    # TODO: tmp config file is never removed
+
+
+def xbindkeys_reload_config_from_disk() -> None:
+    _run_subprocess("killall -HUP xbindkeys")
+
+
+def xbindkeys_killall() -> None:
+    _run_subprocess("killall xbindkeys")
--- xsetwacom.py
+++ xsetwacom.py
@@ -4,113 +4,160 @@ import os
 
 from src.ConfigLoader import ConfigLoader
 from src.DeviceTypeName import DeviceTypeName
+from src.base_config import BaseConfig
 from src.geometry_utils import AreaToOutputMappingMode, map_area_to_output
 from src.tablet_config_utils import configure_devices, print_all_device_parameters
 from src.tablet_utils import print_devices, plot_pressure_curve, plot_current_pressure, get_device_id, get_devices_id
-from src.xbindkeys_utils import run_xbindkeys
+from src.xbindkeys_utils import xbindkeys_start_foreground, xbindkeys_start_background, xbindkeys_reload_config_from_disk, xbindkeys_killall
 
 
 class Env(object):
     def __init__(self):
-        self.script_dir = os.path.dirname(__file__)
-        self.configs_path_name = "configs"
+        self.script_abs_dir = os.path.dirname(__file__)
+        self.configs_rel_path_name = "configs"
 
 
 class Args(object):
-    def __init__(self, env: Env, config_loader: ConfigLoader):
-        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    def __init__(self, config_loader: ConfigLoader):
+        self.parser: argparse.ArgumentParser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        sub_parsers = self.parser.add_subparsers(dest='command', title="command (required)", description="Run command with the loaded configuration.")
 
-        layout_group = parser.add_argument_group("Digitizer")
-        layout_group.add_argument("-d", "--devices",
-                                  help="list devices (i.e. stylus, eraser, touch, pad)",
-                                  action="store_true")
-        layout_group.add_argument("-p", "--parameter",
-                                  help="List all current device(s) parameter by device-id (digitizer must be attached). Device '-' denotes any device.",
-                                  choices=(["-"] + get_devices_id(".*", DeviceTypeName.ANY)))
-        layout_group.add_argument("-w", "--xsetwacom",
-                                  help="Applies configuration to digitizer; see --config",
-                                  action="store_true")
-        layout_group.add_argument("-g", "--geometry",
-                                  help="Modifies the input area according to the current display geometry."
-                                       "next: map to next display, input area is trimmed to retain horizontal vs. vertical proportions;"
-                                       "next_scale: map to next display, input area mapped to output (causes distortion width:height ratio of display is not same as digitizer ratio)",
-                                  choices=(["next", "next_scale"]),
-                                  default="next")
+        sp = sub_parsers.add_parser("device",
+                                    help="detect devices; set and get device parameter",
+                                    description="Digitizer specific actions (see also 'xsetwacom --help').")
+        g = sp.add_mutually_exclusive_group()
+        g.add_argument("-l", "--list",
+                       help="List all discovered devices (i.e. attached: stylus, eraser, touch, pad).",
+                       action="store_true")
+        g.add_argument("-s", "--set",
+                       help="Applies loaded configuration to the digitizer.",
+                       action="store_true")
+        g.add_argument("-m", "--map",
+                       help="Modifies the device input area according to the current display geometry. "
+                            "'next': map to next display, input area is trimmed to retain horizontal vs. vertical proportions. "
+                            "'next_scale': map to next display, input area mapped to output (causes distortion width:height ratio of display is not same as digitizer ratio)",
+                       choices=(["next", "next_scale"]),
+                       default="next")
+        g.add_argument("-p", "--parameter",
+                       help="List all current device(s) parameter by device-id (digitizer must be attached). Device '-' denotes any device.",
+                       choices=(["-"] + get_devices_id(".*", DeviceTypeName.ANY)))
 
-        export_group = parser.add_argument_group("Xbindkeys (start, stop, reload)")
-        export_group.add_argument("-x", "--xbindkeys",
-                                  help="Start (fg, bg) 'xbindkeys' and run in foreground (fg), detach to background (bg), kill (ki) running instances or reload (re) configuration."
-                                       "Xbindkeys will intercept mouse/key events ant trigger actions accordingly. "
-                                       "Applies Xbindkeys configuration as specified by --config."
-                                       "Does not change device parameters.",
-                                  choices=["fg", "bg", "re", "ki"],
-                                  default="fg")
+        sp = sub_parsers.add_parser("bindkeys",
+                                    help="bind device-key events to system mouse/keyboard events",
+                                    description="Xbindkeys will intercepts device-events and triggers system mouse/key events accordingly "
+                                                "(see also 'man xbindkeys').")
+        g = sp.add_mutually_exclusive_group()
+        g.add_argument("-s", "--start",
+                       help="Start Xbindkeys and run in foreground (press CTRL+C to stop).",
+                       action="store_true")
+        g.add_argument("-b", "--background",
+                       help="Start Xbindkeys and run detached to background.",
+                       action="store_true")
+        g.add_argument("-r", "--reload",
+                       help="Tell running Xbindkeys instances to reload configuration from disk without restarting the process.",
+                       action="store_true")
+        g.add_argument("-k", "--kill",
+                       help="Kills running Xbindkeys instances.")
 
-        config_group = parser.add_argument_group("Configuration")
-        config_group.add_argument("-c", "--config",
-                                  help="Apply given configuration for the current run.",
-                                  choices=[c.config_name for c in config_loader.config_names()],
-                                  default="krita_intuos_pro")
-        config_group.add_argument("-s", "--configs",
-                                  help="list known configurations",
-                                  action="store_true")
-        config_group.add_argument("-r", "--print",
-                                  help="print configuration",
-                                  choices=[c.config_name for c in config_loader.config_names()])
+        sub_group = self.parser.add_argument_group("Configuration",
+                                                   description="Load and provide the configuration to the command.")
+        g = sub_group.add_mutually_exclusive_group()
+        g.add_argument("-c", "--config",
+                       help="Loads the given configuration by name.",
+                       choices=[c.config_name for c in config_loader.config_names()],
+                       default="krita_intuos_pro")
 
-        config_group = parser.add_argument_group("Pressure Curve")
-        config_group.add_argument("-u", "--curve",
-                                  help="Plot the configured pressure curve and the resulting Bezier curve (requires gnuplot).",
-                                  action="store_true")
-        config_group.add_argument("-e", "--pressure",
-                                  help="Live plot the current pressure curve (requires xinput and feedgnuplot). The pressure plot does not appear until the first pressure value is reported.",
-                                  action="store_true")
+        sp = sub_parsers.add_parser("cfg",
+                                    help="print known configurations or configuration values",
+                                    description="Print configuration names or read and print values of a specific configuration.")
+        g = sp.add_mutually_exclusive_group()
+        g.add_argument("-l", "--list",
+                       help="List known configurations name and exit.",
+                       action="store_true")
+        g.add_argument("-p", "--print",
+                       help="Print configuration values and exit.",
+                       choices=[c.config_name for c in config_loader.config_names()])
 
-        self.args: argparse.Namespace = parser.parse_args()
+        sp = sub_parsers.add_parser("plot",
+                                    help="visualize pressure and pen curve",
+                                    description="Visualizes the pen curve (static) or the current pen pressure (live).")
+        g = sp.add_mutually_exclusive_group()
+        g.add_argument("-c", "--curve",
+                       help="Plot the configured pressure curve and the resulting Bezier curve (requires gnuplot).",
+                       action="store_true")
+        g.add_argument("-p", "--pressure",
+                       help="Live plot the current pressure curve (requires xinput and feedgnuplot). "
+                            "The pressure plot does not appear until the first pressure value is reported.",
+                       action="store_true")
 
-        if self.args.configs:
-            print("configs:")
-            for config_name in config_loader.config_names():
-                print(f"  - {config_name.config_name} in {os.path.join(env.script_dir, env.configs_path_name)}")
-            exit(0)
+        self.args: argparse.Namespace = self.parser.parse_args()
 
 
-def run():
-    env = Env()
-    config_loader = ConfigLoader(env.script_dir, env.configs_path_name)
-    args = Args(env, config_loader).args
+class Runner(object):
+    def __init__(self):
+        self.env = Env()
+        self.config_loader = ConfigLoader(self.env.script_abs_dir, self.env.configs_rel_path_name)
+        self._cli_args = Args(self.config_loader)
+        self._config = None
 
-    if args.devices:
-        print_devices()
-    elif args.parameter:
-        device_id = None if args.parameter == "-" else args.parameter
-        print_all_device_parameters(device_id)
-    elif args.print:
-        config_loader.load_config(args.print)
-        config_loader.config.print_config()
-    elif args.curve:
-        config_loader.load_config(args.config)
-        config = config_loader.config
-        plot_pressure_curve(config.pressure_curve[DeviceTypeName.STYLUS])
-    elif args.pressure:
-        config_loader.load_config(args.config)
-        config = config_loader.config
-        device_id = get_device_id(config.device_hint_expression, DeviceTypeName.STYLUS)
-        assert device_id
-        plot_current_pressure(device_id)
-    elif args.xsetwacom:
-        config_loader.load_config(args.config)
-        config = config_loader.config
-        configure_devices(config)
-    elif args.geometry:
-        config_loader.load_config(args.config)
-        config = config_loader.config
-        map_area_to_output(config.device_hint_expression, AreaToOutputMappingMode.TRIMMED_INPUT_AREA_FULL_DISPLAY)
-    elif args.xbindkeys:
-        config_loader.load_config(args.config)
-        config = config_loader.config
-        run_xbindkeys(config.xbindkeys_config_string, args.xbindkeys)
+    @property
+    def args(self):
+        return self._cli_args.args
+
+    @property
+    def parser(self):
+        return self._cli_args.parser
+
+    @property
+    def config(self) -> BaseConfig:
+        if not self._config:
+            self.config_loader.load_config(self.args.config)
+            self._config = self.config_loader.config
+        return self.config_loader.config
+
+    def run(self) -> int:
+
+        if not self.args.command:
+            self.parser.print_help()
+            return 1
+
+        if self.args.command == "cfg":
+            if self.args.list:
+                print("known configs:")
+                for config_name in self.config_loader.config_names():
+                    print(f"  - {config_name.config_name} in {os.path.join(self.env.script_abs_dir, self.env.configs_rel_path_name)}")
+            if self.args.print:
+                self.config_loader.config.print_config()
+
+        if self.args.command == "device":
+            if self.args.list:
+                print_devices()
+            if self.args.set:
+                configure_devices(self.config)
+            if self.args.map:
+                map_area_to_output(self.config.device_hint_expression, AreaToOutputMappingMode.TRIMMED_INPUT_AREA_FULL_DISPLAY)
+            if self.args.parameter:
+                device_id = None if self.args.parameter == "-" else self.args.parameter
+                print_all_device_parameters(device_id)
+
+        if self.args.command == "bindkeys":
+            if self.args.start:
+                xbindkeys_start_foreground(self.config.xbindkeys_config_string)
+            if self.args.background:
+                xbindkeys_start_background(self.config.xbindkeys_config_string)
+            if self.args.reload:
+                xbindkeys_reload_config_from_disk()
+            if self.args.kill:
+                xbindkeys_killall()
+
+        if self.args.command == "plot":
+            if self.args.curve:
+                plot_pressure_curve(self.config.pressure_curve[DeviceTypeName.STYLUS])
+            if self.args.pressure:
+                plot_current_pressure(get_device_id(self.config.device_hint_expression, DeviceTypeName.STYLUS))
+
+        return 0
 
 
 if __name__ == "__main__":
-    run()
+    exit(Runner().run())
